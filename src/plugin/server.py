57d56994d82efbb77d738cfccf4d87623ac6780f
jbleyel <jbleyel@gmx.net>
2025-03-14 18:55:06 +0100

    [server.py] * covert to tabs * improve imports
    [e2utils.py]
    * covert to tabs
    * improve imports
    * refactor getAspect -> Thanks to @ruipisco

--- src/plugin/server.py
+++ src/plugin/server.py
@@ -1,50 +1,50 @@
-from __future__ import print_function
-import os
-import logging
-import socketserver
-import struct
+from logging import basicConfig, getLogger, ERROR
+from os import getenv
+from socketserver import BaseRequestHandler, UnixStreamServer
+from struct import calcsize, pack, unpack
 
 try:
-    loglevel = int(os.getenv("E2KODI_DEBUG_LVL", logging.ERROR))
+	loglevel = int(getenv("E2KODI_DEBUG_LVL", ERROR))
 except Exception:
-    loglevel = logging.ERROR
+	loglevel = ERROR
 print("E2KODI_DEBUG_LVL = ", loglevel)
 
-logging.basicConfig(level=loglevel, format='%(name)s: %(message)s',)
-
-class KodiExtRequestHandler(socketserver.BaseRequestHandler):
-
-    def __init__(self, request, client_address, server):
-        self.logger = logging.getLogger('KodiExtRequestHandler')
-        socketserver.BaseRequestHandler.__init__(self, request, client_address, server)
-
-    def handle(self):
-        hlen = struct.calcsize('ibi')
-        header = self.request.recv(hlen)
-        opcode, status, datalen = struct.unpack('ibi', header)
-        if datalen > 0:
-            data = self.request.recv(datalen)
-        else:
-            data = None
-        self.logger.debug('recv()-> opcode = %d, status = %d, data = %s', opcode, status, str(data))
-        status, data = self.handle_request(opcode, status, data)
-        if data is not None:
-            datalen = len(data)
-        else:
-            datalen = 0
-        self.logger.debug('send()-> opcode = %d, status = %d, data = %s', opcode, status, str(data))
-        header = struct.pack('ibi', opcode, status, datalen)
-        self.request.send(header)
-        if datalen > 0:
-            self.request.send(bytes(data, 'utf-8',errors='ignore'))
-
-    def handle_request(self, opcode, status, data):
-        return True, None
+basicConfig(level=loglevel, format='%(name)s: %(message)s',)
 
 
-class UDSServer(socketserver.UnixStreamServer):
+class KodiExtRequestHandler(BaseRequestHandler):
 
-    def __init__(self, server_address, handler_class=KodiExtRequestHandler):
-        self.logger = logging.getLogger('UDSServer')
-        self.allow_reuse_address = True
-        socketserver.UnixStreamServer.__init__(self, server_address, handler_class)
+	def __init__(self, request, client_address, server):
+		self.logger = getLogger('KodiExtRequestHandler')
+		BaseRequestHandler.__init__(self, request, client_address, server)
+
+	def handle(self):
+		hlen = calcsize('ibi')
+		header = self.request.recv(hlen)
+		opcode, status, datalen = unpack('ibi', header)
+		if datalen > 0:
+			data = self.request.recv(datalen)
+		else:
+			data = None
+		self.logger.debug('recv()-> opcode = %d, status = %d, data = %s', opcode, status, str(data))
+		status, data = self.handle_request(opcode, status, data)
+		if data is not None:
+			datalen = len(data)
+		else:
+			datalen = 0
+		self.logger.debug('send()-> opcode = %d, status = %d, data = %s', opcode, status, str(data))
+		header = pack('ibi', opcode, status, datalen)
+		self.request.send(header)
+		if datalen > 0:
+			self.request.send(bytes(data, 'utf-8', errors='ignore'))
+
+	def handle_request(self, opcode, status, data):
+		return True, None
+
+
+class UDSServer(UnixStreamServer):
+
+	def __init__(self, server_address, handler_class=KodiExtRequestHandler):
+		self.logger = getLogger('UDSServer')
+		self.allow_reuse_address = True
+		UnixStreamServer.__init__(self, server_address, handler_class)
